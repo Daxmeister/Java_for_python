dcade8b9878e4a856462727c8183c6656be2c82c
Davide Mario Attebrant Sbrzesny <dmas@kth.se>
2024-01-24 14:37:17 +0100

    First commit

---
+++ Lab1/src/davide/HelloWorld.java
@@ -0,0 +1,12 @@
+package davide;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+
+	}
+
+}

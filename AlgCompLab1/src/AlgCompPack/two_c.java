85a49189112889208d37e0706e9069e5e25067a0
Davide Mario Attebrant Sbrzesny <dmas@kth.se>
2024-02-07 17:10:43 +0100

    Done lab1 except for timing

--- AlgCompLab1/src/AlgCompPack/two_c.java
+++ AlgCompLab1/src/AlgCompPack/two_c.java
@@ -1,21 +1,13 @@
 package AlgCompPack;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-
 public class two_c {
-	private static Integer n = 20;
-	private static Integer k = 4;
-	private static Double p = 0.42;
-	private static Double[] cache;
+	private static int n = 640;
+	private static int k = 236;
+	private static double p = 0.8;
+	private static double[] cache;
 
 	public static void main(String[] args) {
 		Kattio io = new Kattio(System.in, System.out);
-		cache = new Double[n + 1];
-
-		// And call the function
-		System.out.println(winStreak(n));
-		
 
 		while (io.hasMoreTokens()) {
 
@@ -24,9 +16,9 @@ public class two_c {
 			k = io.getInt();
 			p = io.getDouble();
 
-			// Create a cache with room for all values. It will hold value null at every
+			// Create a cache with room for all values. It will hold value 0.0 at every
 			// position.
-			cache = new Double[n + 1];
+			cache = new double[n + 1];
 
 			// And call the function
 			System.out.println(winStreak(n));
@@ -35,28 +27,18 @@ public class two_c {
 
 	}
 
-	private static Double winStreak(Integer x) {
-			if(x<k) {return 0.0;}
-			else if (x==k) {
-				BigDecimal bigP = BigDecimal.valueOf(p);
-				return bigP.pow(k).doubleValue();
-				}
-			else {
-				if(cache[n]==null) {
-					BigDecimal a = BigDecimal.valueOf( winStreak(x-1));
-					BigDecimal b = BigDecimal.valueOf( winStreak(x-k-1));
-					BigDecimal bigP = BigDecimal.valueOf(p);
-					BigDecimal pPowerK = bigP.pow(k);
-					BigDecimal oneMinusP = bigP.negate().add(BigDecimal.valueOf(1.0));
-					
-					a.add(pPowerK.multiply(a));
-					
-					cache[n] = 
-				}
-				return cache[n];
+	private static double winStreak(int x) {
+		if (x < k) {
+			return 0.0;
+		} else if (x == k) {
+			return Math.pow(p, k);
+		} else {
+			if (cache[x] == 0.0) {
+				cache[x] = (winStreak(x - 1) + Math.pow(p, k) * (1 - p) * (1 - winStreak(x - k - 1)));
 			}
-				
-			
+			return cache[x];
+		}
+
 	}
 
 }
